e3b81ffebeac6fab57b09086650e342fc404a5e7
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-09-28 18:53:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Dependency_inversion_principle
+{
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class OrderService
+    {
+        private readonly ILogger _logger;
+
+        public OrderService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void CreateOrder(string order)
+        {
+            // логіка створення
+            _logger.Log("Замовлення створено: " + order);
+        }
+    }
+
+}
+
+
+
